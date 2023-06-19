97f686ccc3c03d165f9a25de876f165588d8fdf6
Nethreshwar <112603835+Nethreshwar@users.noreply.github.com>
2023-06-19 20:02:18 +0530

    Create Analytical1.R

---
+++ Analytical1.R
@@ -0,0 +1,3 @@
+A=matrix(c("Dark","Dark","Dark","dark","Dark","light","light","light","light","light","king","queen","pawn","pawn","knight","bishop","king","rook","pawn","pawn"),nrow=10,ncol=2)
+colnames(A)=c("Player","piece")
+print(A)
